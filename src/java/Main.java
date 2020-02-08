f98ad33c81dfbafdbbf7885e0d0e0c8fd99a6655
alex <alex-habba@mail.ru>
2020-02-09 02:32:50 +0300

    2 changes

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("0000");
+        Controller controller = new Controller();
+        controller.execute();
+    }
+}
+
+interface ModelLayer {
+    Student getStudent();
+}
+
+class Student {
+    String name = "Max";
+    int age = 25;
+}
+
+class DbLayer implements ModelLayer {
+
+    @Override
+    public Student getStudent() {
+        return new Student();
+    }
+}
+
+interface View {
+    void showStudent(Student student);
+}
+
+class ConsoleView implements View {
+
+    @Override
+    public void showStudent(Student student) {
+        System.out.println(student.name + "  " + student.age);
+
+    }
+}
+
+class Controller {
+    ModelLayer modelLayer = new DbLayer();
+    View view = new ConsoleView();
+    void execute() {
+        Student student = modelLayer.getStudent();
+        view.showStudent(student);
     }
 }
